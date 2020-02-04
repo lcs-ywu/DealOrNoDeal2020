51af2cc918de2e252be6016cf74e543589bf1cf5
Russell Gordon <github@russellgordon.ca>
2020-02-04 13:47:49 -0500

    Initial Commit

---
+++ DealOrNoDeal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DealOrNoDeal
+//
+//  Created by Gordon, Russell on 2020-02-04.
+//  Copyright © 2020 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
